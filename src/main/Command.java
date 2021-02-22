22116ce5fd832b885f9c634b8e5c4ff65e405b50
Ana Jimenez <anajimenez.tw@packlink.com>
2021-02-22 08:53:26 +0100

    MarsRover: vector as attribute

--- src/main/Command.java
+++ src/main/Command.java
@@ -2,72 +2,54 @@ package main;
 
 import java.util.HashMap;
 
-public abstract class Command {
-    abstract HashMap<Direction, Vector> getVectorByDirection();
+public  class Command {
+    private HashMap<Direction, Vector> vectorByDirection;
+
+    public Command(HashMap<Direction, Vector> vectorByDirection) {
+        this.vectorByDirection = vectorByDirection;
+    }
 
     MarsRover move(MarsRover rover) {
-        Vector vector = this.getVectorByDirection().get(rover.getDirection());
+        Vector vector = vectorByDirection.get(rover.getDirection());
         return vector.applyVector(rover);
     }
 
-    ;
 
-    public final static Command F = new Command() {
-
-        @Override
-        HashMap<Direction, Vector> getVectorByDirection() {
-            return new HashMap<Direction, Vector>() {
-                {
-                    super.put(Direction.N, new Vector(0, 1, null));
-                    super.put(Direction.S, new Vector(0, -1, null));
-                    super.put(Direction.E, new Vector(1, 0, null));
-                    super.put(Direction.W, new Vector(-1, 0, null));
-                }
-            };
+    public final static Command F = new Command(new HashMap<Direction, Vector>() {
+        {
+            super.put(Direction.N, new Vector(0, 1, null));
+            super.put(Direction.S, new Vector(0, -1, null));
+            super.put(Direction.E, new Vector(1, 0, null));
+            super.put(Direction.W, new Vector(-1, 0, null));
         }
-    };
+    });
 
-    public final static Command B = new Command() {
-        @Override
-        HashMap<Direction, Vector> getVectorByDirection() {
-            return new HashMap<Direction, Vector>() {
-                {
-                    super.put(Direction.N, new Vector(0, -1, null));
-                    super.put(Direction.S, new Vector(0, 1, null));
-                    super.put(Direction.E, new Vector(-1, 0, null));
-                    super.put(Direction.W, new Vector(1, 0, null));
-                }
-            };
+    public final static Command B = new Command(new HashMap<Direction, Vector>() {
+        {
+            super.put(Direction.N, new Vector(0, -1, null));
+            super.put(Direction.S, new Vector(0, 1, null));
+            super.put(Direction.E, new Vector(-1, 0, null));
+            super.put(Direction.W, new Vector(1, 0, null));
         }
-    };
+    });
 
-    public final static Command R = new Command() {
-        @Override
-        HashMap<Direction, Vector> getVectorByDirection() {
-            return new HashMap<Direction, Vector>() {
-                {
-                    super.put(Direction.N, new Vector(0, 0, Direction.E));
-                    super.put(Direction.S, new Vector(0, 0, Direction.W));
-                    super.put(Direction.E, new Vector(0, 0, Direction.S));
-                    super.put(Direction.W, new Vector(0, 0, Direction.N));
-                }
-            };
+    public final static Command R = new Command(new HashMap<Direction, Vector>() {
+        {
+            super.put(Direction.N, new Vector(0, 0, Direction.E));
+            super.put(Direction.S, new Vector(0, 0, Direction.W));
+            super.put(Direction.E, new Vector(0, 0, Direction.S));
+            super.put(Direction.W, new Vector(0, 0, Direction.N));
         }
-    };
+    });
 
-    public final static Command L = new Command() {
-        @Override
-        HashMap<Direction, Vector> getVectorByDirection() {
-            return new HashMap<Direction, Vector>() {
-                {
-                    super.put(Direction.N, new Vector(0, 0, Direction.W));
-                    super.put(Direction.S, new Vector(0, 0, Direction.E));
-                    super.put(Direction.E, new Vector(0, 0, Direction.N));
-                    super.put(Direction.W, new Vector(0, 0, Direction.S));
-                }
-            };
+    public final static Command L = new Command(new HashMap<Direction, Vector>() {
+        {
+            super.put(Direction.N, new Vector(0, 0, Direction.W));
+            super.put(Direction.S, new Vector(0, 0, Direction.E));
+            super.put(Direction.E, new Vector(0, 0, Direction.N));
+            super.put(Direction.W, new Vector(0, 0, Direction.S));
         }
-    };
+    });
 
 }
 
