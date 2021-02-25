3bbc38527945d5b5e6fe6ed2bec48a6c8869e1f6
Ana Jimenez <anajimenez.tw@packlink.com>
2021-02-25 08:42:18 +0100

    MarsRover: rfactor apply limits

--- src/main/PositionMovement.java
+++ src/main/PositionMovement.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class PositionMovement implements Movement {
     private int x;
     private int y;
@@ -18,20 +15,13 @@ public class PositionMovement implements Movement {
         int newY = this.y + rover.getPoint().getY();
 
         Point newPoint = new Point(applyLimitsInMars(newX), applyLimitsInMars(newY));
-
         return new MarsRover(newPoint, rover.getDirection());
     }
 
     private int applyLimitsInMars(int coordinate) {
-        if (coordinate == 181) {
-            return -179;
-        } else if (coordinate == -180) {
-            return 180;
-        } else {
-            return coordinate;
-        }
-
-
+        if (coordinate == 181) return -179;
+        if (coordinate == -180) return 180;
+        return coordinate;
     }
 
 
